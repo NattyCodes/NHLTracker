e4450d31142251b54bb75aa254d351b99e617942
NattyCodes <nathan.romphf@gmail.com>
2024-08-18 20:20:25 -0600

    Implement main page and delete redundant client folder

---
+++ app/hello/page.js
@@ -0,0 +1,8 @@
+export default async function helloPage() {
+    return (
+    <div>
+        <h1>Hey Stinky</h1>
+        <img src="https://assets.nhle.com/logos/nhl/svg/TOR_light.svg" />
+    </div>    
+    );
+}
---
+++ app/page.js
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+import "./globals.css";
+import styles from './Home.module.css';
+export default async function Home() {
+  const data = await fetch('https://api-web.nhle.com/v1/standings/2024-04-18'); 
+  const jsonData = await data.json();
+  const standings = jsonData.standings;
+  return (
+    <main className={styles.main}>
+      <table className={styles.table}>
+        <thead className={styles.head}>
+        <tr>
+          <th>Logo</th>
+          <th>Team</th>
+          <th>P</th>
+          <th>W</th>
+          <th>L</th>
+          <th>OT</th>
+          <th>GP</th>
+        </tr>
+        </thead>
+        <tbody>
+          {Array.isArray(standings) && standings.map(team =>
+            <tr key={team.teamAbbrev.default} className={styles.row}>
+              <td><Image src={team.teamLogo} width={40} height={40} alt={team.teamAbbrev.default}></Image></td>
+              <td>{team.teamCommonName.default}</td>
+              <td>{team.points}</td>
+              <td>{team.wins}</td>
+              <td>{team.losses}</td>
+              <td>{team.otLosses}</td>
+              <td>{team.gamesPlayed}</td>
+            </tr>)}
+        </tbody>
+      </table>
+    </main>
+  );
+}
