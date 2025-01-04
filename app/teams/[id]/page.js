bec7493137f9fc741ca1256c2f63960d659bd2f4
NattyCodes <nathan.romphf@gmail.com>
2025-01-04 12:22:02 -0700

    Move skaters client side to allow transition to a player profile on click

--- app/teams/[id]/page.js
+++ app/teams/[id]/page.js
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import React from 'react';
 import "../../../styles/globals.css";
 import styles from "../../../styles/Home.module.css";
+import Skaters from "./skaters.js";
 
 const Teams = async ({params, searchParams}) => {
     const res = await fetch('https://api-web.nhle.com/v1/club-stats/'+ params.id +'/now');
@@ -24,32 +25,13 @@ const Teams = async ({params, searchParams}) => {
     <div>
         <Image src={logo} width={40} height={40} alt={ params.id }></Image>
         <h1>{name}</h1>
-        <table className={styles.table}>
-        <thead className={styles.head}>
-          <tr>
-            <th></th>
-            <th>Player</th>
-            <th>Position</th>
-            <th>Points</th>
-            <th>Goals</th>
-            <th>Assists</th>
-          </tr>
-        </thead>
-        {Array.isArray(skaters) && skaters.map(skater =>
-        <tr key={skater.playerID} className={styles.row}>
-          <td><Image src={skater.headshot} width={60} height={60} alt={skater.firstName.default}></Image></td>
-          <td>{skater.firstName.default} {skater.lastName.default}</td>
-          <td>{skater.positionCode}</td>
-          <td>{skater.points}</td>
-          <td>{skater.goals}</td>
-          <td>{skater.assists}</td>
-        </tr>)}
-        </table>
+        <Skaters skaters={skaters}></Skaters>
         {Array.isArray(goalies) && goalies.map(goalie =>
         <div key={goalie.playerID}>
           <Image src={goalie.headshot} width={60} height={60} alt={goalie.firstName.default}></Image>
           <p>{goalie.firstName.default} {goalie.lastName.default}</p>
         </div>)}
+        
     </div>
   )
 }
---
+++ app/teams/[id]/skaters.js
@@ -0,0 +1,40 @@
+'use client'
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import "../../../styles/globals.css";
+import styles from "../../../styles/Home.module.css";
+
+const Skaters = (props) =>{
+    let skaters = props.skaters
+    const router = useRouter()
+    return (
+        <table className={styles.table}>
+            <thead className={styles.head}>
+            <tr>
+                <th></th>
+                <th>Player</th>
+                <th>Position</th>
+                <th>Points</th>
+                <th>Goals</th>
+                <th>Assists</th>
+            </tr>
+            </thead>
+            <tbody>
+                {Array.isArray(skaters) && skaters.map(skater =>
+                <tr key={skater.playerID} className={styles.row} onClick={() => {
+                router.push(`../player/${skater.playerId}`);
+                }}>
+                <td><Image src={skater.headshot} width={60} height={60} alt={skater.firstName.default}></Image></td>
+                <td>{skater.firstName.default} {skater.lastName.default}</td>
+                <td>{skater.positionCode}</td>
+                <td>{skater.points}</td>
+                <td>{skater.goals}</td>
+                <td>{skater.assists}</td>
+                </tr>)}
+            </tbody>
+        </table>
+    )
+}
+
+export default Skaters;
