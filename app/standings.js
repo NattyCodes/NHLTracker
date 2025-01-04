ef06d8cc701616a8451851c2974a7c854123fdfc
NattyCodes <nathan.romphf@gmail.com>
2025-01-04 10:32:33 -0700

    Add search params to pass team name from standings to teams page

--- app/standings.js
+++ app/standings.js
@@ -7,12 +7,20 @@ import styles from '../styles/Home.module.css';
 const Standings = (props) => {
   const standings = props.standings
   const router = useRouter()
+
+  const createQueryString = (name, value) => {
+    const params = new URLSearchParams();
+    params.set(name, value);
+
+    return params.toString();
+  }
+
   return (
     <tbody>
           {Array.isArray(standings) && standings.map(team =>
             <tr key={team.teamAbbrev.default} className={styles.row} onClick={() => {
-                router.push(`/teams/${team.teamAbbrev.default}?name=${team.teamName.default}`)
-            }}>
+                router.push(`/teams/${team.teamAbbrev.default}?` + createQueryString("name", team.teamName.default));
+          }}>
               <td><Image src={team.teamLogo} width={40} height={40} alt={team.teamAbbrev.default}></Image></td>
               <td>{team.teamName.default}</td>
               <td>{team.points}</td>
